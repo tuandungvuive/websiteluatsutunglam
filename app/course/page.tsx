33a1906cffff4a5c1ba0edd795100d1a05cdf656
Ben2811 <luanchuong51@gmail.com>
2025-08-27 17:47:02 +0700

    add course page

---
+++ app/course/page.tsx
@@ -0,0 +1,10 @@
+import CoursePage from "@/components/coursepage/CoursePage";
+
+export default function Courses() {
+  return (
+    <>
+      <hr className="border-t-2 border-gray-500" />
+      <CoursePage />
+    </>
+  );
+}
